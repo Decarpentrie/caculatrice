64d5c8cfb5273b3fd2a08ead66d8f52e7d0c5d93
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-03-11 17:29:57 +0100

    1er commit

---
+++ app.js
@@ -0,0 +1,35 @@
+
+$("#bt").onclick("");
+
+
+
+/*
+var a= 70
+var b= 10
+
+function addition(a, b){
+	
+};
+console.log(a + b);
+
+function substracion(a, b){
+
+};	
+
+console.log(a - b);
+
+function multiplication (a,b){
+
+};
+console.log(a * b);
+
+function division (a, b){
+	return a / b
+}
+if (b > 0) {
+	console.log(a / b);
+}
+else{
+	console.log("v")
+};
+*/
